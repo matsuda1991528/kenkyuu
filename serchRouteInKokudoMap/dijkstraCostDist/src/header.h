b443a2bca6f168bedf5873cf529fcb8801c76af3
matsuda1991528 <matsuda1991528@yahoo.co.jp>
2015-04-24 19:41:16 +0900

    first commit

---
+++ serchRouteInKokudoMap/dijkstraCostDist/src/header.h
@@ -0,0 +1,44 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define MAX_LEN 256
+#define EMPTY -1
+#define INF -1
+
+
+/* xy座標を表現する構造体 */
+struct xy_coord_t{
+	double x;  //x座標
+	double y;  //y座標
+};
+
+struct node_t{
+	int num;  //ノード番号
+	struct xy_coord_t pos;  //ノード座標
+};
+
+struct adj_list_t{
+	int num;
+	struct adj_list_t *next;
+};
+
+/* 頂点リストを表現する構造体 */
+struct vertex_t{
+	struct node_t node;
+	struct adj_list_t *adj_list_p;
+	struct adj_list_t *adj_list_head;
+	struct adj_list_t *adj_list_old;
+};
+
+
+/* プロトタイプ宣言 */
+/* commonFunc.c */
+void fileOpenCheck(FILE**, char*, char*);
+
+/* createAdjajencyList.c */
+int loadNodeSize(void);
+void initVertex(struct vertex_t*, int);
+void loadNodeData(struct vertex_t*, int);
+void printVertex(struct vertex_t*, int);
+
+#endif
